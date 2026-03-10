a0ca98586763afeade9f816415d2c43ca648e9e3
Mansi Ganga Nayak <mnayak12@asu.edu>
2026-03-10 01:14:01 -0700

    build: ensure base path is set for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb'], // This tells Vite to treat 3D models as static assets
+  base: '/XR-and-Cybersecurity-Portfolio/',
+  assetsInclude: ['**/*.glb'],
   server: {
     port: 3000,
-    open: true // Automatically opens your browser when you run the app
+    open: true
   }
 });
